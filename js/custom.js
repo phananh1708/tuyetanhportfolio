3936ad715d676346cfc72a904c37c527685b4777
phananh1708 <tuyetanh.blog@gmail.com>
2024-12-10 01:35:37 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+var typed = new Typed('.element', {
+               	strings: ["Web Designer.", "UI-UX Designer.","Apps Designer." ,"HTML- CSS."],
+       	 	typeSpeed:50,
+        		backSpeed:50,
+        		loop:true,
+        		});
+
+  new WOW().init();
+1
